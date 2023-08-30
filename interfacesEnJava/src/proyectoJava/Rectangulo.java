e3fcb98d6ba4ba00a76a7e94fe10d87bc1ee7708
Dani0810 <sharit456z@gmail.com>
2023-08-30 17:58:38 -0500

    extendiendo figuras geometricas

--- interfacesEnJava/src/proyectoJava/Rectangulo.java
+++ interfacesEnJava/src/proyectoJava/Rectangulo.java
@@ -1,4 +1,13 @@
 package proyectoJava;
 
-public class Rectangulo {
+public class Rectangulo extends FigurasGeometricas{
+    @Override
+    double hallarArea() {
+        return 0;
+    }
+
+    @Override
+    double hallarPerimetro() {
+        return 0.2;
+    }
 }
